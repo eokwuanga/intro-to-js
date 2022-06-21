a4af1130ca7ec85852e52d31565f6c578e2c89f6
eokwuanga <105169806+eokwuanga@users.noreply.github.com>
2022-06-21 10:55:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-var nameString = function (name) {return "Hi, I am" + " " + name;};
-var name = nameString ("Emmanuel")
-nameString ("Emmanuel"); console.log (name);
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <script>
+      var nameString = function (name) {
+        return " " + "Hi, My name is" + " " + name + ",";
+      };
+      var name = nameString("Emmanuel");
+      nameString("Emmanuel");
+      document.write(name);
 
-var heightString = function (height) {return "I am" + " " + height + " " + "tall";};
-var height = heightString ("182cm")
-nameString ("182cm"); console.log (height);
+      var heightString = function (height) {
+        return " " + "I am" + " " + height + " " + "tall,";
+      };
+      var height = heightString("182cm");
+      heightString("182cm");
+      document.write(height);
 
-var countryString = function (country) {return "I am from" + " " + country;};
-var country = countryString ("Nigeria")
-nameString ("Nigeria"); console.log (country);
+      var countryString = function (country) {
+        return " " + "and I am from" + " " + country;
+      };
+      var country = countryString("Nigeria");
+      countryString("Nigeria");
+      document.write(country);
+    </script>
+  </body>
+</html>
