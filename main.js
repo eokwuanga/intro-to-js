d502aafeb406c13b3cde9d6e940b3632a5aa988a
eokwuanga <105169806+eokwuanga@users.noreply.github.com>
2022-06-21 03:39:16 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+var nameString = function (name) {return "Hi, I am" + " " + name;};
+var name = nameString ("Emmanuel")
+nameString ("Emmanuel"); console.log (name);
+
+var heightString = function (height) {return "I am" + " " + height + " " + "tall";};
+var height = heightString ("182cm")
+nameString ("182cm"); console.log (height);
+
+var countryString = function (country) {return "I am from" + " " + country;};
+var country = countryString ("Nigeria")
+nameString ("Nigeria"); console.log (country);
